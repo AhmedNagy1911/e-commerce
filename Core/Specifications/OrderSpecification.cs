5c36c8905066dc201ddf28f6a463a77ad5abdfd2
AhmedNagy1911 <zamalekahmed196@gmail.com>
2026-08-18 20:56:25 +0300

    Updating the controller to eagerly load in the get methods

--- Core/Specifications/OrderSpecification.cs
+++ Core/Specifications/OrderSpecification.cs
@@ -6,15 +6,15 @@ public class OrderSpecification : BaseSpecification<Order>
 {
     public OrderSpecification(string email) : base(x => x.BuyerEmail == email)
     {
-        //AddInclude(x => x.DeliveryMethod);
-        //AddInclude(x => x.OrderItems);
-        //AddOrderByDescending(x => x.OrderDate);
+        AddInclude(x => x.DeliveryMethod);
+        AddInclude(x => x.OrderItems);
+        AddOrderByDescending(x => x.OrderDate);
     }
 
     public OrderSpecification(string email, int id) : base(x => x.BuyerEmail == email && x.Id == id)
     {
-        //AddInclude("OrderItems");
-        //AddInclude("DeliveryMethod");
+        AddInclude("OrderItems");
+        AddInclude("DeliveryMethod");
     }
 
    // public OrderSpecification(string paymentIntentId, bool isPaymentIntent) : base(x => x.PaymentIntentId == paymentIntentId)
